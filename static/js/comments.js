2b284c88b422fdf43b014730260950bd598d6923
Richard <lcc.richard@gmail.com>
2026-06-11 23:24:03 +0800

    Fix Cusdis SDK call (renderTo instead of nonexistent setData)

--- static/js/comments.js
+++ static/js/comments.js
@@ -1,25 +1,24 @@
 const CUSDIS_APP_ID = "0341b085-6ee3-4c6c-834d-8115abbaef20";
 
+const cusdisThread = document.getElementById("cusdis_thread");
+
 function updateLightboxComments(photo, albumIdParam, albumTitleParam) {
   const pageId = `photo-${photo.id}`;
   const pageUrl = `${window.location.origin}/album.html?id=${albumIdParam}&photo=${photo.id}`;
   const pageTitle = `${albumTitleParam} - ${photo.title || photo.id}`;
 
-  const apply = () => {
-    window.CUSDIS.setData({
-      appId: CUSDIS_APP_ID,
-      pageId,
-      pageTitle,
-      pageUrl,
-    });
-    window.CUSDIS.setTheme("dark");
-  };
+  cusdisThread.setAttribute("data-app-id", CUSDIS_APP_ID);
+  cusdisThread.setAttribute("data-page-id", pageId);
+  cusdisThread.setAttribute("data-page-url", pageUrl);
+  cusdisThread.setAttribute("data-page-title", pageTitle);
 
-  if (window.CUSDIS) {
+  const apply = () => window.CUSDIS.renderTo(cusdisThread);
+
+  if (window.CUSDIS && window.CUSDIS.renderTo) {
     apply();
   } else {
     const timer = setInterval(() => {
-      if (window.CUSDIS) {
+      if (window.CUSDIS && window.CUSDIS.renderTo) {
         clearInterval(timer);
         apply();
       }
